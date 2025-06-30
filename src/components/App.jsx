45d9b41ae295a48c57d665f22cb79be651c67ee6
LayMui <mui_ume@hotmail.com>
2025-06-30 09:21:48 +0800

    Fix the path issue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     setLoading(true);
     
-    fetch('/tasks.json')
+    fetch('../tasks.json')
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
